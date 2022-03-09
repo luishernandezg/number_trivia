e4f1bbfc4b65b0c7dedc6c58a8a00dee9d062bd1
Luis Hernandez <luis.guaicaipuro.h.a@gmail.com>
2022-03-09 10:50:25 -0400

    11 tutorial - bloc implementation

--- lib/features/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/presentation/bloc/number_trivia_bloc.dart
@@ -3,15 +3,48 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../core/util/input_converter.dart';
 import '../../domain/entities/number_trivia.dart';
+import '../../domain/usecases/get_concrete_number_trivia.dart';
+import '../../domain/usecases/get_random_number_trivia.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero.';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc(
+      {required GetConcreteNumberTrivia concrete,
+      required GetRandomNumberTrivia random,
+      required this.inputConverter})
+      : getConcreteNumberTrivia = concrete,
+        getRandomNumberTrivia = random,
+        super(Empty()) {
     on<NumberTriviaEvent>((event, emit) {
+      emit(Empty());
       // TODO: implement event handler
+    });
+
+    on<GetTriviaForConcreteNumber>((event, emit) {
+      final inputEither =
+          inputConverter.stringToUnsignedInteger(event.numberString);
+
+      emit(inputEither.fold(
+        (failure) {
+          return Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+        },
+        // Although the "success case" doesn't interest us with the current test,
+        // we still have to handle it somehow.
+        (integer) => (Empty()),
+      ));
     });
   }
 }
